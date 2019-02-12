aade7b2c05f3663c8abd3524269d88b9ce044643
financialgoblin <aaltio.amos@gmail.com>
2019-02-12 15:22:19 +0200

    Second commit

--- pages/forms.py
+++ pages/forms.py
@@ -5,10 +5,17 @@ class NameForm(forms.Form):
     coid = forms.CharField(label='Company ID', max_length=100)
 
 class create_employee_form(forms.Form):
+	
     fname = forms.CharField(label='First name', max_length=100)
     surname = forms.CharField(label='Surname', max_length=100)
     title = forms.CharField(label='Title', max_length=100)
     function = forms.CharField(label='Function', max_length=100)
+
+    eristeet = forms.IntegerField(label='Eristeet')
+    siivous = forms.IntegerField(label='Siivous')
+    tetsaus = forms.IntegerField(label='Tetsaus')
+    johtaminen = forms.IntegerField(label='Johtaminen')
+    vitsikkyys = forms.IntegerField(label='Vitsikkyys')
 
 class search_employee_form(forms.Form):
 	function = forms.CharField(label='Employee function', max_length=100)
--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
-class employee(models.Model):
+class employees(models.Model):
 	name = models.CharField(max_length = 30)
 	surname = models.CharField(max_length = 30)
 	title = models.CharField(max_length = 60)
 	function = models.CharField(max_length = 30)
+	
+	siivous = models.IntegerField()
+	tetsaus = models.IntegerField()
+	johtaminen = models.IntegerField()
 
 	def __str__(self):
 		return self.name + ' ' + self.surname
