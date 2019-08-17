83425c8e82c5f19b9a29709bb828b1da428616f2
financialgoblin <aaltio.amos@gmail.com>
2019-08-17 20:04:14 +0300

    SQL hakufunktio valmis + Amoksen lisäysjuttuja

--- pages/project_search.py
+++ pages/project_search.py
@@ -14,26 +14,49 @@ def project_search(form):
 	'construction_operation': form.cleaned_data['construction_operation'],
 	'free_description': form.cleaned_data['free_description'],
 	'project_manager': form.cleaned_data['project_manager'],
-	'value_default': 0
 	}
 
-	
+	#if this value is 0, no filters have been applied
+	sum_values = int(form_data['destination_type'])+int(form_data['building_material'])+int(form_data['service'])+int(form_data['construction_operation'])
+	first = True
+	cond_str = ""
+	if sum_values != 0:
+		cond_str = " WHERE"
+		if int(form_data['destination_type']) != 0:
+			if first:
+				first = False
+				cond_str += " destination_type = :value1"
+			else:
+				cond_str += " AND destination_type = :value1"
+		if int(form_data['building_material']) != 0:
+			if first:
+				first = False
+				cond_str += " building_material = :value2"
+			else:
+				cond_str += " AND building_material = :value2"
+		if int(form_data['service']) != 0:
+			if first:
+				first = False
+				cond_str += " service = :value3"
+			else:
+				cond_str += " AND service = :value3"
+		if int(form_data['construction_operation']) != 0:
+			if first:
+				first = False
+				cond_str += " contruction_operation = :value4"
+			else:
+				cond_str += " AND contruction_operation = :value4"
+
 	database = "db.sqlite3"
 	connection = create_connection(database)
 	cursor = connection.cursor()
-	current = cursor.execute(
-	'''SELECT project_name, specific_project_type FROM pages_project 
-	 WHERE destination_type = :value1
-	 AND building_material = :value2
-	 AND service = :value3
-	 AND contruction_operation = :value4 ''',
-	 {
-	  'value0':form_data['value_default'],
-	  'value1':form_data['destination_type'],
-	  'value2':form_data['building_material'],
-	  'value3':form_data['service'],
-	  'value4':form_data['construction_operation']
-	 })
+	current = cursor.execute("SELECT project_name, specific_project_type FROM pages_project" + cond_str,
+	{
+	'value1':form_data['destination_type'],
+	'value2':form_data['building_material'],
+	'value3':form_data['service'],
+	'value4':form_data['construction_operation']
+	})
 
 	return current.fetchall()
 
