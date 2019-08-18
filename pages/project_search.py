da79306a47daf259796a7779bde8687904271711
artturijalli <artturi.jalli@aalto.fi>
2019-08-18 12:14:10 +0300

    clean SQL search functionality

--- pages/project_search.py
+++ pages/project_search.py
@@ -4,37 +4,32 @@ import urllib.parse
 import re
 from sqlite3 import Error
 
+ELEMS_DICT = {
+		"structure_type": ":value1",
+ 		"building_material": ":value2",
+ 		"service": ":value3",
+ 		"construction_operation": ":value4"
+		}
+
+def sum_filters(form_data):
+	sum = 0
+	for filter, _ in ELEMS_DICT.items():
+		sum += int(form_data[filter])
+	return sum
+
 def create_sql_query(form_data):
-	sum_values = int(form_data['structure_type'])+int(form_data['building_material'])+int(form_data['service'])+int(form_data['construction_operation'])
+	sum_values = sum_filters(form_data)
 	first = True
 	cond_str = ""
 	if sum_values != 0:
 		cond_str = " WHERE"
-		if int(form_data['structure_type']) != 0:
-			if first:
-				first = False
-				cond_str += " structure_type = :value1"
-			else:
-				cond_str += " AND structure_type = :value1"
-		if int(form_data['building_material']) != 0:
-			if first:
-				first = False
-				cond_str += " building_material = :value2"
-			else:
-				cond_str += " AND building_material = :value2"
-		if int(form_data['service']) != 0:
-			if first:
-				first = False
-				cond_str += " service = :value3"
-			else:
-				cond_str += " AND service = :value3"
-		if int(form_data['construction_operation']) != 0:
-			if first:
-				first = False
-				cond_str += " construction_operation = :value4"
-			else:
-				cond_str += " AND construction_operation = :value4"
-
+		for filter, value in ELEMS_DICT.items():
+			if int(form_data[filter]) != 0:
+				if first:
+					first = False
+					cond_str += " " + filter +  " = " + value
+				else:
+					cond_str += " AND " + filter + " = " + value
 	return "SELECT project_name, specific_project_type FROM pages_project" + cond_str
 
 def search(form):
