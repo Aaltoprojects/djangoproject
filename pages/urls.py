14f8a0b5f21e1ceb7fb138cf530586fa4a869199
artturijalli <artturi.jalli@aalto.fi>
2019-08-29 22:34:18 +0300

    Remove tree structure (that never existed)

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
 	path('', views.home, name = 'home'),
 	path('luo', views.create_project, name = 'add_project'),
-	path('tree_structure', views.tree_structure, name ='tree_structure')
 ]
