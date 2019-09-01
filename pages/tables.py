0e221aedf1d869c8ebe2e2695b21b3da8a867a77
financialgoblin <aaltio.amos@gmail.com>
2019-09-01 23:26:52 +0300

    Developed table subclass further

--- pages/tables.py
+++ pages/tables.py
@@ -3,7 +3,20 @@ from django_tables2 import TemplateColumn
 import pages.models as models
 
 class SearchResultTable(tables.Table):
+	Lisätiedot = TemplateColumn(template_name = 'snippets/edit_button.html')
 	class Meta:
 		model = models.Project
+		fields = (
+			'project_name',
+			'destination_name',
+			'start_date',
+			'end_date',
+			'structure_type',
+			'building_material',
+			'service',
+			'construction_operation',
+			'specific_project_type',
+			)
+		empty_text = 'Ei tuloksia'
+		show_header = True
 		attrs = {'class': 'table table-bordered'}
-	Muokkaa = TemplateColumn(template_name = 'snippets/edit_button.html')
