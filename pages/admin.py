b7c2462a1b28c581d2c0762c9abe4c1a50de0e46
ViliMer <vili.merilainen@hotmail.com>
2019-10-12 10:03:24 +0300

    Changed admin site header and removed pointless Group category

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 import pages.models
 
 admin.site.register(pages.models.Project)
+admin.site.site_header = 'Osaamispankki Administration'
+admin.site.unregister(Group)
