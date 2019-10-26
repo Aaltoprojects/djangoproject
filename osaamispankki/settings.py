93330a96fffb83fcef289efd6d9a846ba78d1535
ViliMer <vili.merilainen@hotmail.com>
2019-10-26 11:25:37 +0300

    Added allowed hosts

--- osaamispankki/settings.py
+++ osaamispankki/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'jn%d6#nxfkr#2bkovuyhw&cwf_0ne_5fg-dre&hyrd^dtq+#lt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'arcane-temple-76036.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
