5f9cac9665f70cb88e2e7d42b44c123fff02aa62
ViliMer <vili.merilainen@hotmail.com>
2019-12-13 17:52:33 +0200

    Add filters to adminpage

--- pages/admin.py
+++ pages/admin.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import Group
 import pages.models
 
 admin.site.register(pages.models.Project)
+admin.site.register(pages.models.Filter)
 admin.site.site_header = 'Osaamispankki Administration'
 admin.site.unregister(Group)
