062f6cda24bb3fce8ff4510315f560310360ae8b
financialgoblin <aaltio.amos@gmail.com>
2019-12-14 23:14:21 +0200

    Add placeholder texts for all form fields

--- pages/forms.py
+++ pages/forms.py
@@ -11,23 +11,32 @@ class SearchProjectForm(forms.Form):
     start_date = forms.DateField(
         label='Aikaisin lopetuspäivä',
         required=False,
-        help_text="Projektin aloittamispäivämäärä",
         input_formats=[constants.DATE_FORMAT],
-        widget=FengyuanChenDatePickerInput(attrs={'autocomplete': 'off'}),
+        widget=FengyuanChenDatePickerInput(attrs={
+                                                'placeholder': 'Projektin aloittamispäivämäärä',
+                                                'autocomplete': 'off',
+                                                }
+        ),
     )
     end_date = forms.DateField(
         label='Myöhäisin aloituspäivä',
         required=False,
-        help_text="Projektin lopettamispäivämäärä",
         input_formats=[constants.DATE_FORMAT],
-        widget=FengyuanChenDatePickerInput(attrs={'autocomplete': 'off'}),
+        widget=FengyuanChenDatePickerInput(attrs={
+                                                'placeholder': 'Projektin lopettamispäivämäärä',
+                                                'autocomplete': 'off',
+                                                }
+        ),
     )
     key_phrase_search = forms.CharField(
         label='Avainsanahaku',
         required=False,
-        help_text="Etsi projekteja syöttämällä hakusanoja",
         max_length=100,
-        widget=forms.TextInput(attrs={'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Etsi projekteja syöttämällä hakusanoja', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
 
 
@@ -35,67 +44,88 @@ class CreateProjectForm(forms.Form):
 
     project_name = forms.CharField(
         label='Projektin nimi',
-        help_text="Anna projektin nimi",
         empty_value='—',
         max_length=200,
-        widget=forms.TextInput(attrs={'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Anna projektin nimi', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
     destination_name = forms.CharField(
         label='Kohteen nimi',
         required=False,
-        help_text="Anna kohteen nimi",
         empty_value='—',
         max_length=200,
-        widget=forms.TextInput(attrs={'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Anna kohteen nimi', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
     start_date = forms.DateTimeField(
         label='Aloituspäivä',
         required=False,
-        help_text="Projektin aloittamispäivämäärä",
         input_formats=[constants.DATE_FORMAT],
-        widget=FengyuanChenDatePickerInput(attrs={'autocomplete': 'off'}),
+        widget=FengyuanChenDatePickerInput(attrs={
+                                                'placeholder': 'Projektin aloittamispäivämäärä',
+                                                'autocomplete': 'off',
+                                                }
+        ),
     )
     end_date = forms.DateTimeField(
         label='Lopetuspäivä',
         required=False,
-        help_text="Projektin lopettamispäivämäärä",
         input_formats=[constants.DATE_FORMAT],
-        widget=FengyuanChenDatePickerInput(attrs={'autocomplete': 'off'}),
+        widget=FengyuanChenDatePickerInput(attrs={
+                                                'placeholder': 'Projektin lopettamispäivämäärä',
+                                                'autocomplete': 'off',
+                                                }
+        ),
     )
     keywords = forms.CharField(
         label='Avainsanat',
         required=False,
-        help_text="Anna muutamia avainsanoja, joilla projekti on helppo löytää",
         empty_value='—',
         max_length=200,
-        widget=forms.TextInput(
-            attrs={
-                'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Anna muutamia avainsanoja, joilla projekti on helppo löytää', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
     project_description = forms.CharField(
         label='Projektin kuvaus',
-        help_text="Kuvaile vapaasti projektin sisältöä (max 1000 merkkiä)",
         empty_value='—',
         max_length=5000,
-        widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+        widget=forms.Textarea(attrs={
+                                    'cols': 50,
+                                    'rows': 5,
+                                    'placeholder': 'Kuvaile vapaasti projektin sisältöä (max 1000 merkkiä)',
+                                    }
+        ),
     )
     documentation_path = forms.CharField(
         label='Polku tiedostojen sijaintiin',
         required=False,
-        help_text="Kopioi tähän projektikansion polku (esim C:/Ideastructura/Projektit/Projekti-2019)",
         empty_value='—',
         max_length=500,
-        widget=forms.TextInput(
-            attrs={
-                'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Kopioi tähän projektikansion polku (esim C:/Ideastructura/Projektit/Projekti-2019)', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
     project_manager = forms.CharField(
         label='Projektin vetäjä',
         required=False,
-        help_text="Projektipäällikön nimi",
         empty_value='—',
         max_length=100,
-        widget=forms.TextInput(attrs={'autocomplete': 'off'}),
+        widget=forms.TextInput(attrs={
+                                    'placeholder': 'Projektipäällikön nimi', 
+                                    'autocomplete': 'off'
+                                    }
+        ),
     )
 
 
