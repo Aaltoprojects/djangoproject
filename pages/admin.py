3eb981d48e10eaa90376ab6fce2da2323c46417c
artturijalli <artturi.jalli@aalto.fi>
2020-01-04 14:58:39 +0200

    Change Osaamispankki to Projektipankki

--- pages/admin.py
+++ pages/admin.py
@@ -4,5 +4,5 @@ import pages.models
 
 admin.site.register(pages.models.Project)
 admin.site.register(pages.models.Filter)
-admin.site.site_header = 'Osaamispankki Administration'
+admin.site.site_header = 'Projektipankki Administration'
 admin.site.unregister(Group)
