aeed64cc0f395e2697900ecf82c1b066162458b3
artturijalli <artturi.jalli@aalto.fi>
2020-01-24 11:18:31 +0200

    refister file uploads model

--- pages/admin.py
+++ pages/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group
 import pages.models
 
 admin.site.register(pages.models.Project)
+admin.site.register(pages.models.UploadFilesForm)
 admin.site.register(pages.models.Filter)
 admin.site.register(pages.models.ReferenceProject)
 admin.site.site_header = 'Projektipankin ylläpito'
