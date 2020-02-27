02739b994a9bca27655f089d7701eb64715a902c
ViliMer <vili.merilainen@hotmail.com>
2020-02-27 18:15:24 +0200

    Address typo

--- osaamispankki/settings.py
+++ osaamispankki/settings.py
@@ -158,5 +158,5 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = 'ideastructura-s3-bucket-dev'
+AWS_STORAGE_BUCKET_NAME = 'ideastructura-s3-bucked-dev'
 AWS_S3_REGION_NAME = 'eu-north-1'
